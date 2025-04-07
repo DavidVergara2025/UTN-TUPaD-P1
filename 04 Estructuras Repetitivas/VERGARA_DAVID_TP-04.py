ef15f394f764559b568defc8b3f6d0b37b42f008
Your Name <david.e.vergara2025.com>
2025-04-07 11:44:53 -0300

    Ejercicio 2

--- 04 Estructuras Repetitivas/VERGARA_DAVID_TP-04.py
+++ 04 Estructuras Repetitivas/VERGARA_DAVID_TP-04.py
@@ -10,3 +10,18 @@ for i in range (101):
 
 #==========================================================================
 
+#Ejercicio 2: 2) Desarrolla un programa que solicite al usuario un número entero y determine la cantidad de
+#dígitos que contiene.
+
+#Se inicia contador de dígitos en 1 y se pide número al usuario
+cont = 1
+num = int(input("Ingrese un número entero: "))
+
+#Si el número es mayor o igual a 10 positivo o negativo, se divide por 10 y se incrementa un dígito en cada repetición:
+while num >= 10 or num <= -10:
+    num = num/10
+    cont +=1
+#Se imprime la cantidad de dígitos hallados
+print(cont)
+
+#=====================================================================================================
