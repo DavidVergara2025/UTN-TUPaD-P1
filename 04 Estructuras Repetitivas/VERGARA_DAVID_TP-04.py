95275a6bb59bdccb63226b2fa31ff83fa114e713
Your Name <david.e.vergara2025.com>
2025-04-07 12:01:58 -0300

    Ejercicio 3

--- 04 Estructuras Repetitivas/VERGARA_DAVID_TP-04.py
+++ 04 Estructuras Repetitivas/VERGARA_DAVID_TP-04.py
@@ -10,7 +10,7 @@ for i in range (101):
 
 #==========================================================================
 
-#Ejercicio 2: 2) Desarrolla un programa que solicite al usuario un número entero y determine la cantidad de
+#Ejercicio 2: Desarrolla un programa que solicite al usuario un número entero y determine la cantidad de
 #dígitos que contiene.
 
 #Se inicia contador de dígitos en 1 y se pide número al usuario
@@ -25,3 +25,23 @@ while num >= 10 or num <= -10:
 print(cont)
 
 #=====================================================================================================
+
+#Ejercicio 3: Escribe un programa que sume todos los números enteros comprendidos entre dos valores
+#dados por el usuario, excluyendo esos dos valores.
+
+# Se solicitan dos números al usuario:
+num1 = int(input(" Ingrese el primer número entero: "))
+num2 = int(input(" Ingrese el segundo número entero: "))
+
+#Se ordenan los números
+if num1 >num2:
+   mayor = num1
+   num1 = num2
+   num2 = mayor
+
+#Se inicia el acumulador "suma" en 0. Se usa un bucle "for" para repetir la suma de cada número, excluyendo los dos extremos:
+suma = 0
+for i in range (num1+1, num2):
+   suma += i
+
+print(f"La suma de los números entre ambos valores(excluyéndolos) es {suma}")
