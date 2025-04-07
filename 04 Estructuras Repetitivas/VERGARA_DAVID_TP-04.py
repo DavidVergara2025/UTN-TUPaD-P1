1530cef6cee10cc8aaddd2ae0efd184a7fbb7abc
Your Name <david.e.vergara2025.com>
2025-04-07 11:26:37 -0300

    Ejercicio 1

---
+++ 04 Estructuras Repetitivas/VERGARA_DAVID_TP-04.py
@@ -0,0 +1,12 @@
+#TRABAJO PRÁCTICO N°4 - ESTRUCTURAS REPETITIVAS
+#Alumno: Vergara, David
+#Comisión: 5
+
+#Ejercicio 1: Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100
+#(incluyendo ambos extremos), en orden creciente, mostrando un número por línea.
+
+for i in range (101):
+    print(i)
+
+#==========================================================================
+
