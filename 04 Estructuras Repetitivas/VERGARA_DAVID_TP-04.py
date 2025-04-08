e421974d2fd122e5d156947b539e1d6b221345c5
Your Name <david.e.vergara2025.com>
2025-04-08 10:11:34 -0300

    Ejercicio 9

--- 04 Estructuras Repetitivas/VERGARA_DAVID_TP-04.py
+++ 04 Estructuras Repetitivas/VERGARA_DAVID_TP-04.py
@@ -149,3 +149,20 @@ Positivos: {pos}
 Negativos: {neg}
 Neutros: {neutro}
 """)
+
+#======================================================================================================================
+
+#Ejercicio 9: Elabora un programa que permita al usuario ingresar 100 números enteros y luego calcule la
+#media de esos valores. (Nota: puedes probar el programa con una cantidad menor, pero debe
+#poder procesar 100 números cambiando solo un valor).
+
+#Se pide al usuario ingresar 100 números enteros, mediante el bucle for, y se suman en cada repetición:
+suma = 0
+for i in range(1,101):
+    num = int(input(f"Ingrese el {i}° número entero: "))
+    suma += num
+
+#Se muestra el resultado en pantalla:
+print(f"La media de los valores ingresados es: {suma/100}")
+
+#======================================================================================================================
