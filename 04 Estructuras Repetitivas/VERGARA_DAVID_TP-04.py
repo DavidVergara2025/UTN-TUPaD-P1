9b2f5c24c456df5fc8b704e024c6958c66187083
Your Name <david.e.vergara2025.com>
2025-04-08 09:27:58 -0300

    Ejercicio 6

--- 04 Estructuras Repetitivas/VERGARA_DAVID_TP-04.py
+++ 04 Estructuras Repetitivas/VERGARA_DAVID_TP-04.py
@@ -87,3 +87,11 @@ print(f"Correcto! Cantidad de intentos: {cont}")
 
 #======================================================================================================================
 
+#Ejercicio 6:Desarrolla un programa que imprima en pantalla todos los números pares comprendidos
+#entre 0 y 100, en orden decreciente.
+for i in range(98,0,-2):
+    print(i)
+
+#======================================================================================================================
+
+
