e5d80bdc2e7b73b3daf3a6cf979c254d14203153
Your Name <david.e.vergara2025.com>
2025-04-15 12:38:03 -0300

    Ejercicio 8

--- 05 Funciones/VERGARA_DAVID_TP-05.py
+++ 05 Funciones/VERGARA_DAVID_TP-05.py
@@ -144,6 +144,24 @@ mostrar_resultados(a, b, operaciones_basicas(a, b))
 
 #----------------------------------------------------------------------------------------------------------------
 
+#Ejercicio 8. Crear una función llamada calcular_imc(peso, altura) que reciba el peso en kilogramos y la altura 
+#en metros, y devuelva el índice de masa corporal (IMC). Solicitar al usuario los datos y llamar a la función
+#para mostrar el resultado con dos decimales   
+
+#FUNCIONES
+def calcular_imc(peso, altura):
+    IMC = peso / altura**2
+    return IMC
+
+#PROGRAMA PRINCIPAL
+
+#Solicitud de datos:
+peso = float(input("Ingrese su peso en kg: "))
+altura = float(input("Ingrese su altura en m: "))
+
+#Se muestra el resultado con solo dos decimales (:.2f)
+print(f"Su IMC es {calcular_imc(peso, altura):.2f}")
+
 #----------------------------------------------------------------------------------------------------------------
 
 #----------------------------------------------------------------------------------------------------------------
