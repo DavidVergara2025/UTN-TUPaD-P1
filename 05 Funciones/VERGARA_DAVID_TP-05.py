7c0112e9d41b83ce92453912663a0abbe0018606
Your Name <david.e.vergara2025.com>
2025-04-15 10:02:06 -0300

    Ejercicio 1

---
+++ 05 Funciones/VERGARA_DAVID_TP-05.py
@@ -0,0 +1,13 @@
+#TRABAJO PRACTICO N°5 - FUNCIONES
+#Alumno: Vergara, David Emanuel
+#Comisión: 5
+
+#Ejercicio 1. Crear una función llamada imprimir_hola_mundo que imprima por pantalla el mensaje: “Hola Mundo!”. 
+# Llamar a esta función desde el programa principal
+
+#Se define la funcion:
+def imprimir_hola_mundo():
+    print("Hola Mundo!")
+
+#Se llama a la función:
+imprimir_hola_mundo()
