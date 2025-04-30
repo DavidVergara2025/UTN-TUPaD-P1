0dbd3da33ef9819d495acd339ebccd633db0f689
David Vergara <david.e.vergara2025@gmail.com>
2025-04-30 10:34:14 -0300

    TP Lisats resuelto

--- 06 Datos complejos/Listas.py
+++ 06 Datos complejos/Listas.py
@@ -5,14 +5,14 @@
 #Ejercicio 1: Crear una lista con los números del 1 al 100 que sean múltiplos de 4. Utilizar la función range.
 
 lista1 = list(range(4,101,4))
-print(lista)
+print(lista1)
 
 #----------------------------------------------------------------------------------------------------------------
 
 #Ejercicio 2) Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el penúltimo. 
 # ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el indexing con números negativos!
 
-lista2 = [a, b, c, d, e]
+lista2 = ["a", "b", "c", "d", "e"]
 print(lista2[-2])
 
 #----------------------------------------------------------------------------------------------------------------
