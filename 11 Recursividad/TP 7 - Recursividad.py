b3f4f23d40c9451038a723b96fda2b9de1d7f3e8
David Vergara <david.e.vergara2025@gmail.com>
2025-05-22 11:54:43 -0300

    7

--- 11 Recursividad/TP 7 - Recursividad.py
+++ 11 Recursividad/TP 7 - Recursividad.py
@@ -112,3 +112,23 @@ def suma_digitos(n):
 n = int(input("Ingrese un numero entero positivo: "))
 print(f"La suma de los digitos de {n} es {suma_digitos(n)}")
         
+#====================================================================================================
+
+#Ejercicio 7:Un niño está construyendo una pirámide con bloques. En el nivel más bajo coloca n bloques, en el siguiente nivel uno menos (n - 1),
+# y así sucesivamente hasta llegar al último nivel con un solo bloque.
+#Escribí una función recursiva contar_bloques(n) que reciba el número de bloques en el nivel más bajo y devuelva el total de bloques que necesita
+# para construir toda la pirámide.
+#Ejemplos:
+#contar_bloques(1) → 1 (1)
+#contar_bloques(2) → 3 (2 + 1)
+#contar_bloques(4) → 10 (4 + 3 + 2 + 1)
+
+def contar_bloques(n):
+    if n == 1:
+        return 1
+    else:
+        return n + contar_bloques(n-1)
+    
+n = int(input("Ingrese el numero de bloques del nivel mas bajo: "))
+print(f"El numero de bloques necesarios es {contar_bloques(n)} bloques")
+
