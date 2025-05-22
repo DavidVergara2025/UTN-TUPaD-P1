afd826ae875104172ae65523b7e6e7cc206dbb8f
David Vergara <david.e.vergara2025@gmail.com>
2025-05-22 11:42:26 -0300

    Ejercicio 6

--- 11 Recursividad/TP 7 - Recursividad.py
+++ 11 Recursividad/TP 7 - Recursividad.py
@@ -91,5 +91,24 @@ if es_palindromo(palabra):
 else:
     print("No es palindromo")
 
+#====================================================================================================
 
+#Ejercicio 6: Escribí una función recursiva en Python llamada suma_digitos(n) que reciba un número entero positivo y devuelva la suma
+# de todos sus dígitos.
+#Restricciones:
+#No se puede convertir el número a string.
+#Usá operaciones matemáticas (%, //) y recursión.
+#Ejemplos:
+#suma_digitos(1234) → 10 (1 + 2 + 3 + 4)
+#suma_digitos(9) → 9
+#suma_digitos(305) → 8 (3 + 0 + 5)
+
+def suma_digitos(n):
+    if n < 10:
+        return n
+    else:
+        return (n % 10) + suma_digitos(n // 10)
+
+n = int(input("Ingrese un numero entero positivo: "))
+print(f"La suma de los digitos de {n} es {suma_digitos(n)}")
         
