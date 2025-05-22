af17470662ae8687dd9de4b58d924f806c673306
David Vergara <david.e.vergara2025@gmail.com>
2025-05-22 12:53:43 -0300

    TP completo

--- 11 Recursividad/TP 7 - Recursividad.py
+++ 11 Recursividad/TP 7 - Recursividad.py
@@ -132,3 +132,24 @@ def contar_bloques(n):
 n = int(input("Ingrese el numero de bloques del nivel mas bajo: "))
 print(f"El numero de bloques necesarios es {contar_bloques(n)} bloques")
 
+#====================================================================================================
+
+#Ejercicio 8: Escribí una función recursiva llamada contar_digito(numero, digito) que reciba un número entero positivo (numero) y un dígito 
+#(entre 0 y 9), y devuelva cuántas veces aparece ese dígito dentro del número.
+#Ejemplos:
+#contar_digito(12233421, 2) → 3
+#contar_digito(5555, 5) → 4
+#contar_digito(123456, 7) → 0
+
+def contar_digito(numero, digito):
+    if numero == 0:
+        return 0
+    else:
+        if (numero % 10) == digito:
+            return 1 + contar_digito(numero // 10, digito)
+        else:
+            return contar_digito(numero // 10, digito)
+
+numero = int(input("Ingrese el numero: "))
+digito = int(input("Ingrese el digito: "))
+print(f"El digito {digito} se repite {contar_digito(numero, digito)} veces en {numero}")
