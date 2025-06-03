5402a3034e162f8e913db09d8273f1150c0bb4c6
David Vergara <david.e.vergara2025@gmail.com>
2025-06-03 11:05:09 -0300

    Ejercicios 2 y 3

--- 06 Datos complejos/6_Datos_Complejos.py
+++ 06 Datos complejos/6_Datos_Complejos.py
@@ -15,3 +15,24 @@ precios_frutas['Naranja'] = 1200
 precios_frutas['Manzana'] = 1500
 precios_frutas['Pera'] =  2300
 print(precios_frutas)
+
+#===============================================================================================
+
+#Ejercicio 2: Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código 
+#desarrollado en el punto anterior, actualizar los precios de las siguientes frutas: 
+#● Banana = 1330 
+#● Manzana = 1700 
+#● Melón = 2800 
+
+precios_frutas['Banana'] = 1330
+precios_frutas['Manzana'] = 1700
+precios_frutas['Melón'] =  2800
+print(precios_frutas)
+
+#===============================================================================================
+
+#Ejercicio 3: Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código 
+#desarrollado en el punto anterior, crear una lista que contenga únicamente las frutas sin los 
+#precios. 
+frutas = list(precios_frutas.keys())
+print(frutas)
